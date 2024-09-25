235a6a6668eae6db90ac5efbf95e7c6356ac75a1
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2024-09-25 10:49:21 +0200

    Entering goal loads next scene. Added empty lvl 2

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    int currentLvl = 1;
     public void Start()
     {
         Debug.Log("Goal start");
     }
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other + " entered goal");
+        if (other.name.Equals("Character"))
+        {
+            currentLvl ++;
+            SceneManager.LoadScene("Level" + currentLvl.ToString());
+        }
     }
 }
